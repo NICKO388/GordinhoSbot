6c79d20b41c2d093c961c5a450bfe69bac03bba3
Mr Trump <62407732+Pietro222222@users.noreply.github.com>
2021-01-22 15:55:00 -0300

    adicionado sistema de cancelamento

--- commands/emojis.js
+++ commands/emojis.js
@@ -7,13 +7,43 @@ const Discord = require('discord.js');
  * @param {Discord.Message} msg
  */
 exports.run = (client, msg, args) => {
+    const filter = m => m.content.includes('');
+    
+    let i = 0
+    
     const emojis = msg.guild.emojis.cache.array()   
     console.log(`${emojis.length}`)
-    msg.channel.send(`${msg.author} voce sera spammado com os emojis do servidor`)
-    for(let i = 0; i< emojis.length; i++){
-        const embed = new MessageEmbed()
-        embed.setDescription(`${emojis[i]}`)
-        embed.setTitle(`Emoji ${i} de ${emojis.length}`)
-        msg.author.send(embed)
+    if(args[0] == "-parar"){
+        i = emojis.length + 10;
+        return;
     }
+
+    
+    msg.channel.send(`${msg.author} voce sera spammado com os emojis do servidor. Digite "cancelar" em 30 segundos para cancelar (voce nunca mais tera essa oportunidade)`).then(() => {
+        
+        msg.channel.awaitMessages(filter, {max: 1, time: 30000, errors: [`time`]})
+            .then(collected => {
+                if(collected.first().author.id == msg.author.id){
+                    if(collected.array().join(" ").includes("cancelar")){
+                        i = emojis.length + 10;
+                        msg.channel.send(`Cancelado com sucesso`)
+                        return;
+                    }
+                }
+                
+            }).catch(collected => {
+                msg.channel.send(`Parece que nao cancelaram`)
+                sendEmojis()
+            })
+    })
+    
+    function sendEmojis(){
+        for(i; i< emojis.length; i++){
+            const embed = new MessageEmbed()
+            embed.setDescription(`${emojis[i]}`)
+            embed.setTitle(`Emoji ${i} de ${emojis.length}`)
+            msg.author.send(embed)
+        }
+    }
+    
 }
